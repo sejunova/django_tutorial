c6b19ccb05bbead237612e1e1665dd94905099c9
Sejun Im <sejun425@gmail.com>
2017-09-28 16:21:49 +0900

    model created

---
+++ tutorial/polls/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f'설문조사 {self.title}'
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    title = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'설문조사 {self.question.title} - 선택지 {self.title}'
